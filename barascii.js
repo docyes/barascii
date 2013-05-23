f4b7b3882c9033b1d08e0f4972ab678b95613346
Carl S. Yestrau <carl@featureblend.com>
2013-05-23 14:14:40 -0700

    works on node

--- barascii.js
+++ barascii.js
@@ -1,10 +1,27 @@
-var ticks = ['▁','▂','▃','▄','▅','▆','▇','█'];
-function chart(){
-    var m = Math.min.apply(null, arguments),
-        n = (Math.max.apply(null, arguments) - m) / (ticks.length - 1),
-        bars = [];
-    for (t in arguments) {
-        bars.push(ticks[(arguments[t]-m)/n >> 0]);
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else {
+        root.returnExports = factory();
     }
-    return bars;
-}
+}(this, function () {
+    function Chart(options) {
+        options || (options = {});
+        this.range = options.range || ['▁','▂','▃','▄','▅','▆','▇','█'];
+    }
+    Chart.prototype.plot = function(values, options) {
+        options || (options = {});
+        var m = Math.min.apply(null, values),
+            n = (Math.max.apply(null, values) - m) / (this.range.length - 1),
+            columns = [];
+        for (var i = 0, l = values.length; i < l; i++) {
+            columns.push(this.range[(values[i] - m) / n >> 0]);
+        }
+        return (options.stringify) ? columns.join('') : columns;
+    };
+    return Chart;
+}));
+
+
