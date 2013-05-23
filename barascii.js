c0b083049ecd7e7d1a4502a0e3133eeff01ef47b
Carl S. Yestrau <carl@featureblend.com>
2013-05-23 13:33:57 -0700

    initial c

---
+++ barascii.js
@@ -0,0 +1,10 @@
+var ticks = ['▁','▂','▃','▄','▅','▆','▇','█'];
+function chart(){
+    var m = Math.min.apply(null, arguments),
+        n = (Math.max.apply(null, arguments)-m) / (ticks.length - 1),
+        bars = [];
+    for (t in arguments) {
+        bars.push(ticks[(arguments[t]-m)/n >> 0]);
+    }
+    return bars;
+}
